d52ef32da1219ed0fde5c2fc7e1478fd26154fea
Andrew Loeliger <loeliger@wisc.edu>
2026-01-07 13:12:40 -0600

    Add option for 1 or 3 channel in VAE

--- trainCICADA2026Teacher_VAE.py
+++ trainCICADA2026Teacher_VAE.py
@@ -1,3 +1,5 @@
+import argparse
+
 import numpy as np
 import yaml
 from rich.console import Console
@@ -9,12 +11,15 @@ from src import teacher_vae, trainTeacher
 console = Console()
 
 
-def main(params):
+def main(params, args):
     console.log("Making VAE model")
 
     dataPath = params["inputFiles"]["dataTrainingFile"]
     caloRegions, tauBits, egBits = trainTeacher.loadFile(dataPath)
-    dataGrids = np.concatenate([caloRegions, tauBits, egBits], axis=-1)
+    if args.use3Channels:
+        dataGrids = np.concatenate([caloRegions, tauBits, egBits], axis=-1)
+    else:
+        dataGrids = caloRegions
 
     train_grids, test_grids = train_test_split(
         dataGrids, test_size=0.2, random_state=123
@@ -29,6 +34,11 @@ def main(params):
 
     vae_model.summary()
 
+    if args.use3Channels:
+        model_name = "teacher_model_vae"
+    else:
+        model_name = "teacher_model_3channel_vae"
+
     vae_model.fit(
         x=dataGrids,
         y=dataGrids,
@@ -38,9 +48,9 @@ def main(params):
             keras.callbacks.EarlyStopping(patience=30, restore_best_weights=True),
             keras.callbacks.ReduceLROnPlateau(patience=10),
             keras.callbacks.ModelCheckpoint(
-                "data/teacher_model_vae.keras", store_best_only=True
+                f"data/{model_name}.keras", store_best_only=True
             ),
-            keras.callbacks.CSVLogger("data/training_vae_log.csv"),
+            keras.callbacks.CSVLogger(f"data/{model_name}_log.csv"),
         ],
     )
 
@@ -52,7 +62,13 @@ def main(params):
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Make a VAE model")
+    parser.add_argument(
+        "--use3Channels", action="store_true", help="Create a 3 channel model"
+    )
+    args = parser.parse_args()
+
     with open("params.yaml") as theFile:
         params = yaml.safe_load(theFile)
 
-    main(params)
+    main(params, args)
